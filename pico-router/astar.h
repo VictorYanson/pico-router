2f23deccd01eddeea5a6e12632c190e51ba3c5c0
Victor Yanson <victor.yanson@outlook.com>
2026-04-08 11:48:59 +0000

    feat: add A* class

--- pico-router/astar.h
+++ pico-router/astar.h
@@ -1,4 +1,31 @@
 #pragma once
 
-void reconstruct_path();
-void astar();
+#include <array>
+#include <optional>
+#include <stdint.h>
+
+#include "graph.h"
+#include "heuristic.h"
+#include "path.h"
+#include "priority_queue.h"
+
+/**
+* Basic implementation of A* algorithm
+* Pseudo-code: https://en.wikipedia.org/wiki/A*_search_algorithm#Pseudocode
+*/
+
+class Astar {
+public:
+    // todo: add constructor
+    static constexpr size_t MAX_NODES = 256;
+
+    std::optional<Path> Calculate(const Graph& graph, node_id start, node_id goal);
+
+private:
+    std::array <int32_t, MAX_NODES> gScore;
+    std::array <int32_t, MAX_NODES> fScore;
+
+    PriorityQueue<Node, MAX_NODES> open_list;
+
+    Path reconstruct_path(NodeId current);
+};
