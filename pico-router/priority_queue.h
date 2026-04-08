c9cd4978683964cba22755ea3ec1b6872da05f9c
Victor Yanson <victor.yanson@outlook.com>
2026-04-08 11:45:54 +0000

    temp: preliminary priority queue class

--- pico-router/priority_queue.h
+++ pico-router/priority_queue.h
@@ -4,28 +4,26 @@
 #include <array>
 #include "graph.h"
 
-constexpr size_t MAX_OPEN_NODES = 128;
-std::array<Node, MAX_OPEN_NODES> open_list;
-size_t current_size = 0;
+/** 
+* todo: rewrite this class
+*/
 
-struct CompareNodes {
-    bool operator()(const Node& n1, const Node& n2) {
-        return n1.f > n2.f;
-    }
+template <size_t MAX_OPEN_NODES>
+
+class PriorityQueue {
+public:
+    std::array<Node, MAX_OPEN_NODES> queue;
+
+    struct CompareNodes {
+        bool operator()(const Node& n1, const Node& n2) {
+            return n1.f > n2.f;
+        }
+    };
+
+    void add(Node);
+    void remove(Node);
+    void clear();
+
+private:
+    size_t current_size = 0;
 };
-
-void addToOpenList(Node n)
-{
-    if (current_size < MAX_OPEN_NODES)
-    {
-        open_list[current_size] = n;
-        current_size++;
-        std::push_heap(open_list.begin(), open_list.begin() + current_size, CompareNodes());
-    }
-}
-
-Node popBestNode() {
-    std::pop_heap(open_list.begin(), open_list.begin() + current_size, CompareNodes());
-    current_size--;
-    return open_list[current_size];
-}
