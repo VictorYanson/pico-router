51356d169839fb6d04e0f05e073d277dc9c4577c
Victor Yanson <victor.yanson@outlook.com>
2026-04-08 11:44:28 +0000

    feat: add Graph struct

--- pico-router/graph.h
+++ pico-router/graph.h
@@ -2,15 +2,20 @@
 
 #include <stdint.h>
 
+struct Graph {
+    uint32_t graph_id;
+};
+
 struct Edge {
+    uint32_t edge_id;
     uint16_t to; 
     uint16_t cost;
 };
 
 struct Node {
+    int32_t node_id;
     int32_t x, y;
-    // node gScore, heuristic, fScore
-    // int32_t g, h, f;
+    int32_t g, h, f;
     Edge edges[4];
     uint8_t edge_count;
 };
