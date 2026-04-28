9f2686f7314c13c0b66e0bd5ce6755db1643f597
Victor Yanson <victor.yanson@outlook.com>
2026-04-28 21:11:11 +0200

    feat: add linked list fields for edge exploration

--- include/graph.h
+++ include/graph.h
@@ -11,19 +11,21 @@ static constexpr size_t MAX_NODES = 10000;
 
 struct Edge {
     edge_id id;
-    uint16_t to; 
+    uint16_t to;
     uint16_t cost;
+    uint32_t next_edge_index;
 };
 
 struct Node {
     node_id id;
-    double g_score, f_score;
-    Edge edges[4];
+    int32_t g_score, f_score;
     uint16_t edge_count;
+    uint32_t first_edge_index;
 };
 
 struct Graph {
     graph_id id;
+    //todo: find a better data struct for this
     std::vector<Node> nodes;
 
     const Node* getNode(uint16_t id) const {
